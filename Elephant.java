929de53374cca914f0a636208fc9c8082b98a517
chem1101 <350745600@gapps.yrdsb.ca>
2023-12-14 11:51:27 -0500

    Updated Elephant.java
    animated elephant trunk
    swings fast

--- Elephant.java
+++ Elephant.java
@@ -9,14 +9,29 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     /**
      * Constructor - The code that gets run the first time this object is create
      */
     public Elephant()
     {
-        setImage(idle);
+        for (int i = 0; i<8; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    /**
+     * Animate the elephant
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+        
     }
     
     public void act()
@@ -32,6 +47,9 @@ public class Elephant extends Actor
         
         // remove apple if elephant "eats it"
         eat(); 
+        
+        // Animate the elephant
+        animateElephant();
     }
     
     /** 
