956fdc41f9b760c156f094d0fae09ac376d4c464
chem1101 <350745600@gapps.yrdsb.ca>
2023-12-14 12:55:41 -0500

    Updated TitleScreen.java
    press space to start

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleLabel = new Label("Hungry Elephant", 80);
+    Label instruction = new Label("press space to start",40);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -18,8 +19,14 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject(titleLabel, 300, 200);
+        
+        // added instructions to titleScreen
+        addObject(instruction, 300, 300);
     }
     
+    /**
+     * The main world loop
+     */
     public void act()
     {
         //starts the game if the user presses the space bar
